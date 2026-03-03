97adb069db41d2cd0e7bc336c606f81e44097f49
Virkkunen <vrkknn@vrkknn.net>
2026-03-03 10:46:19 +0100

    fix: dont show space rooms in home tab

--- src/app/pages/client/home/useHomeRooms.ts
+++ src/app/pages/client/home/useHomeRooms.ts
@@ -1,7 +1,15 @@
 import { useAtomValue } from 'jotai';
+import { useMemo } from 'react';
 import { allRoomsAtom } from '../../../state/room-list/roomList';
+import { useMatrixClient } from '../../../hooks/useMatrixClient';
+import { isSpace } from '../../../utils/room';
 
 export const useHomeRooms = () => {
+  const mx = useMatrixClient();
   const rooms = useAtomValue(allRoomsAtom);
-  return rooms;
+
+  return useMemo(
+    () => rooms.filter((roomId) => !isSpace(mx.getRoom(roomId))),
+    [mx, rooms]
+  );
 };
